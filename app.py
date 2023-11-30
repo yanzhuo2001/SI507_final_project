d63ff16d8f2c1c6b367f245745500894dcb29d03
yanzhuo2001 <143744751+yanzhuo2001@users.noreply.github.com>
2023-11-30 15:36:05 +0800

    apply both search and filter at the same time

--- app.py
+++ app.py
@@ -1,21 +1,20 @@
 from flask import Flask, render_template, request, jsonify
-import json
 import pandas as pd
 from convert_json_map import load_data, search_restaurants, filter_restaurants, show_restaurant_details, create_map
 
+app = Flask(__name__)
+
 def format_restaurants(data):
-    # Check if data is a list and convert to DataFrame if necessary
     if isinstance(data, list):
         data = pd.DataFrame(data)
 
-    # Format the results for the template
+    # 将 NaN 值替换为 'N/A'
+    data.fillna("N/A", inplace=True)
+
     formatted_results = []
     for _, row in data.iterrows():
-        # Format categories as a comma-separated string
         formatted_categories = '; '.join([category['title'] for category in row['categories']])
-        # Format transactions as a comma-separated string, if they exist
         formatted_transactions = '; '.join(row['transactions']) if 'transactions' in row and row['transactions'] else 'None'
-        # Add a dictionary for each restaurant with the desired information
         formatted_results.append({
             'name': row['name'],
             'categories': formatted_categories,
@@ -23,11 +22,10 @@ def format_restaurants(data):
             'price': row['price'],
             'review_count': row['review_count'],
             'transactions': formatted_transactions,
-            'url': row['url']  # 添加 Yelp 网址
+            'url': row['url'],
+            'coordinates': row['coordinates']
         })
     return formatted_results
-
-app = Flask(__name__)
 
 # 加载数据和初始化地图
 G, df = load_data('ann_arbor_restaurants.json')
@@ -35,46 +33,87 @@ create_map(df, 'static/map.html')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    page = request.args.get('page', 1, type=int)
+    per_page = 10
+    start = (page - 1) * per_page
+    end = start + per_page
+    paginated_data = df.iloc[start:end]
+    formatted_results = format_restaurants(paginated_data)
+    total_pages = (len(df) + per_page - 1) // per_page
+
     if request.method == 'POST':
         query = request.form['search_query']
         results = search_restaurants(df, query)
-        # Format the results for the template
         formatted_results = format_restaurants(results)
         create_map(results, 'static/map.html')
-        return render_template('index.html', restaurants=formatted_results)
-    else:
-        # Format the results for the template
-        formatted_results = format_restaurants(df)
-        create_map(df, 'static/map.html')
-        return render_template('index.html', restaurants=formatted_results)
 
-@app.route('/filter', methods=['POST'])
-def filter_restaurants_route():
-    # Process the filtering request
-    categories = request.form.get('categories', '').split(',')
-    price = request.form.get('price', None)
-    rating = request.form.get('rating', None)
+    return render_template('index.html', restaurants=formatted_results, total_pages=total_pages, current_page=page)
+
+@app.route('/api/restaurants', methods=['GET'])
+def api_restaurants():
+    page = request.args.get('page', 1, type=int)
+    query = request.args.get('query', '')
+    categories = request.args.get('categories', '')
+    price = request.args.get('price', '')
+    rating = request.args.get('rating', None)
     rating = float(rating) if rating else None
-    review_count = request.form.get('review_count', None)
+    review_count = request.args.get('review_count', None)
     review_count = int(review_count) if review_count else None
-    transactions = request.form.get('transactions', '').split(',')
-    sort_by = request.form.get('sort_by', None)
-    categories = [c.strip() for c in categories if c.strip()]
-    transactions = [t.strip() for t in transactions if t.strip()]
+    transactions = request.args.get('transactions', '')
+    sort_by = request.args.get('sort_by', '')
 
-    # Call the filter_restaurants function with the processed input
-    results = filter_restaurants(df, categories, price, rating, review_count, transactions, sort_by)
-    # Format the results for the template
-    formatted_results= format_restaurants(results)
-    # Save the map with the filtered results
-    create_map(results, 'static/map.html')
-    # Pass the formatted results to the template
-    return render_template('index.html', restaurants=formatted_results)
+    # 初始筛选为全部数据
+    filtered_df = df.copy()
 
+    # 根据查询字符串过滤
+    if query:
+        filtered_df = filtered_df[filtered_df['name'].str.contains(query, case=False)]
+
+    # 根据分类过滤
+    if categories:
+        category_list = [category.strip() for category in categories.split(';')]
+        filtered_df = filtered_df[filtered_df['categories'].apply(lambda cats: any(cat['title'] in category_list for cat in cats))]
+
+    # 根据价格过滤
+    if price:
+        filtered_df = filtered_df[filtered_df['price'] == price]
+
+    # 根据评级过滤
+    if rating:
+        filtered_df = filtered_df[filtered_df['rating'] >= rating]
+
+    # 根据评论数量过滤
+    if review_count:
+        filtered_df = filtered_df[filtered_df['review_count'] >= review_count]
+
+    # 根据交易类型过滤
+    if transactions:
+        transaction_list = [transaction.strip() for transaction in transactions.split(';')]
+        filtered_df = filtered_df[filtered_df['transactions'].apply(lambda trans: any(t in transaction_list for t in trans))]
+
+    # 实现排序
+    if sort_by:
+        filtered_df = filtered_df.sort_values(by=sort_by)
+
+    create_map(filtered_df, filename='static/map.html')
+
+    # 实现分页
+    per_page = 10
+    start = (page - 1) * per_page
+    end = start + per_page
+    paginated_data = filtered_df.iloc[start:end]
+    formatted_results = format_restaurants(paginated_data)
+    total_pages = (len(filtered_df) + per_page - 1) // per_page
+
+    return jsonify({
+        'restaurants': formatted_results,
+        'total_pages': total_pages,
+        'current_page': page,
+        'map_url': 'static/map.html'  # 添加地图 URL
+    })
 
 @app.route('/details/<int:index>', methods=['GET'])
 def restaurant_details(index):
-    # 显示餐厅详细信息
     restaurant = show_restaurant_details(df, index)
     return jsonify(restaurant.to_dict())
 
